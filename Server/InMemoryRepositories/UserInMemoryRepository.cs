3495f1ff8ffb5ed7765d166fb95d286e0aab8476
detrimend <rasmusduus19@gmail.com>
2025-09-08 18:29:36 +0200

    Part 1 complete (minus GetSingleAsync impl.)

---
+++ Server/InMemoryRepositories/UserInMemoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class UserInMemoryRepository : IUserRepository
+{
+    private List<User> users;
+
+    public Task<User> AddAsync(User user)
+    {
+        user.Id = users.Any()
+            ? users.Max(u => u.Id) + 1
+            : 1;
+        users.Add(user);
+        return Task.FromResult(user);
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        User? exisitingUser = users.SingleOrDefault(u => u.Id == user.Id);
+        if (exisitingUser is null)
+        {
+            throw new InvalidOperationException(
+                $"User with ID '{user.Id}' not found");
+        }
+
+        users.Remove(exisitingUser);
+        users.Add(user);
+
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        User? userToRemove = users.SingleOrDefault(u => u.Id == id);
+        if (userToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"User with ID '{id}' not found");
+        }
+
+        users.Remove(userToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<User> GetSingleAsync(int id)
+    {
+        throw new NotImplementedException();
+        //return Task.FromResult(user);
+    }
+
+    public IQueryable<User> GetMany()
+    {
+        return users.AsQueryable();
+    }
+}
